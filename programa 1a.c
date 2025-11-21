e35700ab5282a983f08b3bb4faadd398f36b1a60
slipclotmc <slipclotmc8@gmail.com>
2025-11-21 10:00:55 -0600

    Create programa 1a.c

---
+++ programa 1a.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int matriz[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+ int i, j; 
+  
+ printf("Imprimir Matriz\n");  
+  
+ for (i=0 ; i<3 ; i++) //Representa al renglón del arreglo
+   {
+     for (j=0 ; j<3 ; j++)//Representa a la columna del arreglo 
+     {
+       printf("%d, ",matriz[i][j]);
+     }
+     printf("\n");
+   }
+ return 0;
+}
